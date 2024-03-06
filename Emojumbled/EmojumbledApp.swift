9563fe4ee4c0c41cad4125d9b187780b3debe7c0
siraajul <shirajulislamparvez@gmail.com>
2024-03-07 05:37:26 +0600

    Initial Commit

---
+++ Emojumbled/EmojumbledApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojumbledApp.swift
+//  Emojumbled
+//
+//  Created by Sirajul Islam on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojumbledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
